f03acd442054b1fd2581a0ed7bbd11bb4267f9c2
Kibika <steshykibika@gmail.com>
2022-03-10 16:18:40 -0800

    use puts to ptint

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 /**
- *  * main - prints a string
- *   * @parameterx: no parameter input
- *   (* a blank line
- *    * Description: Longer description of the function)?
- *    (* section header: Section description)*
- *     * Return: 0 because input is int
- *      */
-
-int main(void) 
+ * main - prints a string
+ * Description: Use puts to print string
+ * Return: 0 because input is int
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
