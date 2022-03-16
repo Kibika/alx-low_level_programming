a822d72ce946e4f42d4231b1521abaac58331c88
Kibika <steshykibika@gmail.com>
2022-03-16 12:31:30 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,8 +11,12 @@ void print_to_98(int n)
 	{
 		while (n < 99)
 		{
-		printf("%d, ", n);
-		n++;
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
 		}
 		printf("\n");
 	}
@@ -20,7 +24,11 @@ void print_to_98(int n)
 	{
 		while (n > 97)
 		{
-			printf("%d, ", n);
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
 			n--;
 		}
 		printf("\n");
