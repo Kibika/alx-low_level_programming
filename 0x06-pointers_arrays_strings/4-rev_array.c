df909f149d8fcb51572cc64b9ca08b9af0676759
Kibika <steshykibika@gmail.com>
2022-03-24 14:17:01 -0700

    reverse string

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,10 +9,10 @@ void reverse_array(int *a, int n)
 {
 	int i, j;
 
-	for (j = 0; j < n; j++)
+	for (j = 0; j < (n / 2); j++)
 	{
 		i = a[j];
-		a[j] = a[n-i-1];
-		a[n-i-1] = i;
-	}	
+		a[j] = a[n - i - 1];
+		a[n - i - 1] = i;
+	}
 }
