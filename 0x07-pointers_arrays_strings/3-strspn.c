2002877f3efbb85ce9a310d732da10106e275163
Kibika <steshykibika@gmail.com>
2022-03-29 07:28:19 -0700

    get length of substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,10 +7,11 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = 0, j = 0;
+	unsigned int i, j = 0;
 
 	while (s[j] != '\0')
 	{
+		i = 0;
 		while (accept[i] != '\0')
 		{
 			if (accept[i] == s[j])
