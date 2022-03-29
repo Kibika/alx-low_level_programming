69627422e35424c5dc968d7cc30c48b0e0635e02
Kibika <steshykibika@gmail.com>
2022-03-29 07:23:03 -0700

    get length of substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,18 +1,25 @@
 #include "main.h"
 /**
- *
+ * _strspn - get length of substring
+ * @s: string
+ * @accept: substring
+ * Return: len of substring in string
  */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i = 0, j = 0;
 
-	while ((*(s + j) != 0) && (*(accept + i) != 0))
+	while (s[j] != '\0')
 	{
-		if (*(accept + i) == *(s + j))
+		while (accept[i] != '\0')
 		{
+			if (accept[i] == s[j])
+			{
+				;
+			}
 			i++;
-			j++;
 		}
+		j++;
 	}
-	return (j);
+	return (i + 1);
 }
