ceed78a8a53537acedc6ce5f81e03bf5c3598132
Kibika <steshykibika@gmail.com>
2022-04-11 19:38:29 -0700

    dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog properties
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
