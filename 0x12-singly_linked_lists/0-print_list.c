e3020a926de814475fe767c66e884aa8db18bfa7
Kibika <steshykibika@gmail.com>
2022-04-24 19:24:07 -0700

    print elements of list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,12 +14,15 @@ size_t print_list(const list_t *h)
 	c = 0;
 	current = h;
 	if (current == NULL)
-		printf("[%d] %s\n", 0, "(nil)");
-	while (current != NULL)
+		printf("[%d] %s\n", current->len, "(nil)");
+	else
 	{
-		printf("[%d] %s\n", current->len, current->str);
-		current = current->next;
-		c++;
+		while (current != NULL)
+		{
+			printf("[%d] %s\n", current->len, current->str);
+			current = current->next;
+			c++;
+		}
 	}
 
 	return (c);
