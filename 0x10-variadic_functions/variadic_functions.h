0d641f38ec77ed61ab3b4d1a55e691adebc0cfa8
Kibika <steshykibika@gmail.com>
2022-05-21 01:36:12 -0700

    print

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,19 +1,18 @@
-#ifndef VARFUNC
-#define VARFUNC
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-/**
- * struct typ - Struct operators
- *
- * @c: Char
- * @tp: The function associated
- */
-typedef struct typ
+void _printchar(va_list list);
+void _printstr(va_list list);
+void _printfloat(va_list list);
+void _printint(va_list list);
+
+typedef struct checker
 {
-	char *c;
-	void (*tp)(va_list var);
-} typ_t;
+	char *type;
+	void (*f)();
+} checker;
 #endif
