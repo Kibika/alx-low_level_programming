cbfd1e97fd5cfbaa32f209506eb6bfa7b2810bfe
Kibika <steshykibika@gmail.com>
2022-06-10 12:53:22 -0700

    insert dlist end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- *  * add_dnodeint_end - Add a new node at the end of a `dlistint_t` list
- *   * @head: double pointer to head node
- *    * @n: value to store in new node
- *     * Return: Address of new element, or NULL if it failed
- *      */
+ * add_dnodeint_end - Add a new node at the end of a `dlistint_t` list
+ * @head: double pointer to head node
+ * @n: value to store in new node
+ * Return: Address of new element, or NULL if it failed
+ */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *tmp, *new;
